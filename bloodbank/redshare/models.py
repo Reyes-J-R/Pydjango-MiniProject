9e8d319591a10ebc3eb89a32615844c3b366c90f
Reyes-J-R <58396982+Reyes-J-R@users.noreply.github.com>
2021-10-05 19:35:20 +0530

    Delete bloodbank directory

--- bloodbank/redshare/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Member(models.Model):
-	name = models.CharField(max_length=20)
-	blood_group = models.CharField(max_length=5)
-	phone_number = models.CharField(max_length=15)
-	place = models.CharField(max_length=50)
